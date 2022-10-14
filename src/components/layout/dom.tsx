659570f39a697c4087a410fd21371e5c02c64368
Sebastian Nunez <02snunez@gmail.com>
2022-10-14 14:43:01 -0400

    Updated main container for the page (non-3d)

--- src/components/layout/dom.tsx
+++ src/components/layout/dom.tsx
@@ -1,26 +1,26 @@
-import useStore from "@/helpers/store"
-import { useEffect, useRef } from "react"
+import useStore from "@/helpers/store";
+import { useEffect, useRef } from "react";
 
 const Dom = ({ children }) => {
-	const ref = useRef(null)
-	// useEffect(() => {
-	//   useStore.setState({ dom: ref })
-	// }, [])
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-top">
+        {children}
+      </main>
 
-	return (
-		<div
-			className="dom"
-			style={{
-				position: "absolute",
-				top: 0,
-				left: 0,
-				zIndex: 10,
-				overflow: "hidden",
-			}}
-			ref={ref}>
-			{children}
-		</div>
-	)
-}
+      <footer className="flex h-20 w-full items-center mt-14 justify-center border-t-2">
+        <a
+          className="flex items-center justify-center gap-2 cursor-pointer"
+          href="https://upe.cs.fiu.edu/sparkdev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <strong className="text-pink-500">SparkDev</strong> |
+          Copyright &copy; 2022
+        </a>
+      </footer>
+    </div>
+  );
+};
 
-export default Dom
+export default Dom;
