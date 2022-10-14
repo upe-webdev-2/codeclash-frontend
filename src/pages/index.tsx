208dab2d705e9e5d16071180b8e4b966cde36a50
styled <smyvensestime@gmail.com>
2022-10-14 14:32:41 -0400

    refractor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import useStore from "@/helpers/store"
-import dynamic from "next/dynamic"
-import { useRouter } from "next/router"
+import useStore from "@/helpers/store";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 // import Shader from '@/components/canvas/ShaderExample/ShaderExample'
 
 // Prefer dynamic import for production builds
@@ -8,55 +8,55 @@ import { useRouter } from "next/router"
 // comment these out and import above instead
 // https://github.com/pmndrs/react-three-next/issues/49
 const Shader = dynamic(
-	() => import("@/components/canvas/ShaderExample/ShaderExample"),
-	{
-		ssr: false,
-	}
-)
+  () => import("@/components/canvas/ShaderExample/ShaderExample"),
+  {
+    ssr: false,
+  }
+);
 
 // DOM elements here
 const DOM = () => {
-	const { router } = useStore()
-	const handleOnClick = () => {
-		router.push("/playground")
-	}
-	return (
-		<>
-			<h1>This is the landing page</h1>
-			<button onClick={handleOnClick}>Button to playground</button>
-		</>
-	)
-}
+  const { router } = useStore();
+  const handleOnClick = () => {
+    router.push("/playground");
+  };
+  return (
+    <>
+      <h1 className="uppercase underline">This is the landing page</h1>
+      <button onClick={handleOnClick}>Button to playground</button>
+    </>
+  );
+};
 
 // Canvas/R3F components here
 const R3F = () => {
-	// Example of using the router to change pages
-	// It can also be inside R3F component (see `two.tsx` and `Box.tsx`)
-	const { router } = useStore()
-	const handleOnClick = () => {
-		router.push("/two")
-	}
+  // Example of using the router to change pages
+  // It can also be inside R3F component (see `two.tsx` and `Box.tsx`)
+  const { router } = useStore();
+  const handleOnClick = () => {
+    router.push("/two");
+  };
 
-	return (
-		<>
-			<Shader onClick={handleOnClick} />
-		</>
-	)
-}
+  return (
+    <>
+      <Shader onClick={handleOnClick} />
+    </>
+  );
+};
 
 export default function Page() {
-	return (
-		<>
-			<DOM />
-			<R3F />
-		</>
-	)
+  return (
+    <>
+      <DOM />
+      <R3F />
+    </>
+  );
 }
 
 export async function getStaticProps() {
-	return {
-		props: {
-			title: "Welcome!",
-		},
-	}
+  return {
+    props: {
+      title: "Welcome!",
+    },
+  };
 }
