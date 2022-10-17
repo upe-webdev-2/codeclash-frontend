aac999c3c8d5ae55d84ecd46a30b2e7f7e8ccf20
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2022-10-17 17:01:12 -0400

    playground

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ const config = {
   title: `CodeClash`,
   url: `https://github.com/upe-webdev-2`,
   description:
-    "This is an interactive coding enviroment where players can battle it out through Data Structures and Alogorithms questions!",
+    "This is an interactive coding enviroment where players can battle it out through Data Structures and Algorithms questions!",
   author: "SparkDev - Web Dev II",
   keywords: ["CodeClash", "1vs1", "leetcode", "hackerrank"],
   twitter: "@UPEFIU"
