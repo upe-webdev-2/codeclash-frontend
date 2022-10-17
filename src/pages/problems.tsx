423c6d668eedea059a85d26a460a90f697b7077b
Sebastian Nunez <02snunez@gmail.com>
2022-10-17 15:39:10 -0400

    Basic problems page implementation.
    - Created basic layout for the problems page.
    - Once the user clicks on a problem, they will be redirected to /problems/id

--- src/pages/problems.tsx
+++ src/pages/problems.tsx
@@ -1,6 +1,8 @@
 import useStore from "@/helpers/store";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Link from "next/link";
 // import Shader from '@/components/canvas/ShaderExample/ShaderExample'
 
 // Prefer dynamic import for production builds
@@ -13,13 +15,58 @@ const Shader = dynamic(
     ssr: false
   }
 );
+const URL = "http://localhost:8000";
 
 // DOM elements here
-const DOM = () => {
+const DOM = ({ problems }) => {
   return (
     <>
-      <h1 className="text-6xl font-bold mb-5 pt-10 px-6">Problems Page :/</h1>
-      <p className="text-xl text-pink-600">Working on it...</p>
+      <div className="flex flex-col w-4/5 mt-5">
+        <table className="min-w-full">
+          <thead className="bg-white border-b">
+            <tr>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                ID
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                Title
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                Difficulty
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {problems.map((problem, index) => {
+              return (
+                <Link key={index} href={`/problems/${problem.id}`}>
+                  <tr className="bg-white border-b transition duration-300 ease-in-out hover:cursor-pointer hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {problem.id}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {problem.title}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {problem.difficulty}
+                    </td>
+                  </tr>
+                </Link>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
@@ -29,19 +76,23 @@ const R3F = () => {
   return <></>;
 };
 
-export default function leaderboard() {
+export default function problems(props) {
   return (
     <>
-      <DOM />
+      <DOM {...props} />
       {/* <R3F /> */}
     </>
   );
 }
 
 export async function getStaticProps() {
+  const res = await fetch(`${URL}/problems`);
+  const data = await res.json();
+
   return {
     props: {
-      title: "Problems"
+      title: "Problems",
+      problems: data
     }
   };
 }
