54c391569fa73577343a588db91ef2d7b7bc4e5b
02danieljonas <02danieljonas@gmail.com>
2022-11-05 14:13:04 -0400

    turned logo into svg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import NavElements from "./NavElements";
+import ProfileComponents from "./ProfileComponents";
 
 const Navbar = () => {
+  let isLoggedIn = true;
   return (
     <div className="flex justify-between w-full pt-1 px-28 text-lg">
       <div className="flex flex-col">
         <Image
-          src={"/static/CodeClashLogo.png"}
+          src={"/static/logo.svg"}
           alt="Image of Code Clash Logo"
           width={"101.94px"}
           height={"101.88px"}
@@ -16,7 +18,7 @@ const Navbar = () => {
           Code<span className="font-bold">Clash</span>
         </p>
       </div>
-      <NavElements/>
+      {isLoggedIn ? <ProfileComponents /> : <NavElements />}
     </div>
   );
 };
