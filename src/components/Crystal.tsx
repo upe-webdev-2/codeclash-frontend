2f2da4acd987bf3480683dc0f399691b828410c8
02danieljonas <02danieljonas@gmail.com>
2022-11-06 13:34:05 -0500

    Crystal.tsx bug, custom width removed

--- src/components/Crystal.tsx
+++ src/components/Crystal.tsx
@@ -8,16 +8,22 @@ type Crystal = {
   height: string;
 };
 
-const Crystal = ({ alt, className, width, height }: Crystal) => (
-  <div className={`h-[${height}] w-[${width}] relative `}>
-    <Image
-      className={`${className} object-contain`}
-      alt={`${alt || "image of xp"}`}
-      src="/static/xp.svg"
-      layout="fill"
-    />
-    <div className="bottom-[-10px] absolute content-[''] w-[180%] left-[-50%] h-[40%] rounded-[200px/50px] bg-gradient-radial from-[#739AFF40] via-[#739AFF05] to-transparent"></div>
-  </div>
-);
+const Crystal = ({ alt, className, width, height }: Crystal) => {
+  console.log(width);
+  //TODO error with passing width
+  //. sometimes tailwind will ignore the width prop sometimes it won't ignore
+  //. lots of headache has been caused because of that
+  return (
+    <div className={`h-[${height}] w-[3rem] relative `}>
+      <Image
+        className={`${className} object-contain`}
+        alt={`${alt || "image of xp"}`}
+        src="/static/xp.svg"
+        layout="fill"
+      />
+      <div className="bottom-[-10px] absolute content-[''] w-[180%] -left-2/4 h-[40%] rounded-[200px/50px] bg-gradient-radial from-[#739AFF40] via-[#739AFF05] to-transparent"></div>
+    </div>
+  );
+};
 
 export default Crystal;
