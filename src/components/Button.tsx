47d73a4c241c04e0fbc112096292779d09498c74
02danieljonas <02danieljonas@gmail.com>
2022-11-06 16:11:28 -0500

    added customizability to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,18 +5,32 @@ type Button = {
   onclick: () => void;
   type: "fill" | "outline" | "none";
   movingGradient: boolean;
+  width?: string;
+  height?: string;
 };
 
-const Button = ({ name, onclick, type, movingGradient }: Button) => (
+const Button = ({
+  name,
+  onclick,
+  type,
+  movingGradient,
+  width,
+  height
+}: Button) => (
   <button
     onClick={onclick}
-    className={`rounded-[30px] transition-all duration-1000 bg-primary cursor-pointer ${
-      type !== "none" && "bg-gradient-to-r p-[3px] from-tertiary to-secondary "
-    } ${movingGradient && " bg-size-200 bg-pos-0 hover:bg-pos-100"}`}
+    className={`${height ? `h-${height} ` : " "} ${width ? `w-${width} ` : " "} 
+      rounded-[30px] transition-all duration-1000 bg-primary cursor-pointer 
+        ${
+          type !== "none" &&
+          "bg-gradient-to-r p-[3px] from-tertiary to-secondary "
+        } 
+      ${movingGradient && " bg-size-200 bg-pos-0 hover:bg-pos-100 "}
+    `}
   >
     <div
       className={`rounded-[30px] py-3 px-10  ${
-        type !== "fill" && "bg-primary"
+        type !== "fill" && "bg-primary "
       }`}
     >
       {name}
