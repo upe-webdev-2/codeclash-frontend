44ff8f51f57794e49e7ad9511bd85d4eb889afa1
02danieljonas <02danieljonas@gmail.com>
2022-11-07 10:48:47 -0500

    added inline styling and global css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,41 +1,40 @@
-import React from "react";
+import { ReactNode } from "react";
 
 type Button = {
-  name: string;
+  children: ReactNode;
   onclick: () => void;
   type: "fill" | "outline" | "none";
   movingGradient: boolean;
   width?: string;
   height?: string;
+  style?: {};
 };
 
 const Button = ({
-  name,
+  children,
   onclick,
   type,
   movingGradient,
   width,
-  height
+  height,
+  style = {}
 }: Button) => (
   <button
     onClick={onclick}
-    className={`${height ? `h-${height} ` : " "} ${width ? `w-${width} ` : " "} 
-      rounded-[30px] transition-all duration-1000 bg-primary cursor-pointer 
-        ${
-          type !== "none" &&
-          "bg-gradient-to-r p-[3px] from-tertiary to-secondary "
-        } 
-      ${movingGradient && " bg-size-200 bg-pos-0 hover:bg-pos-100 "}
+    style={{ height, width, ...style }}
+    className={`rounded-[30px] transition-all duration-1000 bg-primary cursor-pointer 
+        ${type !== "none" && " button-gradient "} ${
+      movingGradient && " button-moving-gradient "
+    }
     `}
   >
     <div
       className={`rounded-[30px] py-3 px-10  ${
-        type !== "fill" && "bg-primary "
+        type !== "fill" && " button-outline "
       }`}
     >
-      {name}
+      {children}
     </div>
   </button>
 );
-
 export default Button;
