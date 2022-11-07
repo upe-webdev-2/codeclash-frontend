0b465db8f07c12a9737d2086157e79e7ea0cd3e6
02danieljonas <02danieljonas@gmail.com>
2022-11-07 08:51:47 -0500

    fixed bug, width is now passed as an inline style

--- src/components/Crystal.tsx
+++ src/components/Crystal.tsx
@@ -1,28 +1,23 @@
 import React from "react";
 import Image from "next/image";
+
 type Crystal = {
   alt?: string;
   type?: string;
-  className?: string;
-  width: string;
-  height: string;
+  styles?: {};
+  width?: string;
 };
 
-const Crystal = ({ alt, className, width = "3rem", height }: Crystal) => {
-  // error with passing width
-  // sometimes tailwind will ignore the width prop, causing the Crystal to not appear
-  // lots of headache has been caused because of that
-  return (
-    <div className={`h-[${height}] w-[${width}] relative`}>
-      <Image
-        className={`${className} object-contain `}
-        alt={`${alt || "image of xp"}`}
-        src="/static/xp.svg"
-        layout="fill"
-      />
-      <div className="bottom-[-10px] absolute content-[''] w-[180%] translate-x-[-20%] h-[40%] rounded-[200px/50px] bg-gradient-radial from-[#739AFF40] via-[#739AFF05] to-transparent"></div>
-    </div>
-  );
-};
+const Crystal = ({ alt, styles = {}, width = "3rem" }: Crystal) => (
+  <div className={`relative`} style={{ width: width, ...styles }}>
+    <Image
+      className={`object-contain `}
+      alt={`${alt || "image of xp"}`}
+      src="/static/xp.svg"
+      layout="fill"
+    />
+    <div className="bottom-[-10px] absolute content-[''] w-[180%] translate-x-[-20%] h-[40%] rounded-[200px/50px] bg-gradient-radial from-[#739AFF40] via-[#739AFF05] to-transparent"></div>
+  </div>
+);
 
 export default Crystal;
