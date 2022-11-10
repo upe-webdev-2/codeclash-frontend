a9b8c288433d851beb812c5508b450930d1e2c60
styled <smyvensestime@gmail.com>
2022-11-10 07:22:39 -0500

    change font and color

--- src/templates/Playground/Instructions.tsx
+++ src/templates/Playground/Instructions.tsx
@@ -41,9 +41,7 @@ const Instruction = (problem: Problem) => {
             <div className="flex flex-col gap-2 p-5 my-5 rounded-lg bg-primary">
               {Object.entries(example).map(([key, value], index) => (
                 <h3 key={index}>
-                  <span className="mr-3 text-gray-200 font-poppins">
-                    {key}:{" "}
-                  </span>
+                  <span className="mr-3 font-jetBrains">{key}: </span>
                   <span className="text-gray-600 font-jetBrains">{value}</span>
                 </h3>
               ))}
