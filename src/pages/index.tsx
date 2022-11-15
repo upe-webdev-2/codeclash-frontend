244a62fae7a8ea835a7016cc8d63f3b77c337e1b
02danieljonas <02danieljonas@gmail.com>
2022-11-14 20:33:32 -0500

    removed redirect from signIn()

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ const DOM = () => {
         </>
       ) : (
         <>
-          <button onClick={() => signIn("google")}>Sign In</button>
+          <button onClick={() => signIn()}>Sign In</button>
           <h3>not signed in</h3>
         </>
       )}
