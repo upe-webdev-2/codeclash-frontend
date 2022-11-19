649fb2f09f5733696055d55bbe8f6294bb89567d
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2022-11-18 20:52:03 -0500

    Added Tailwind Styling to Leaderboard
    Added rounded rows
    Added colored background for rows
    Added colored text for ranks
    Added necessary spacing and alignments.
    TODO: Make actual icon of badges appear. Will probably use flex box.

--- src/components/profile/leaderboard/leaderboard-item.tsx
+++ src/components/profile/leaderboard/leaderboard-item.tsx
@@ -5,13 +5,25 @@ import Crystal from "@/components/Crystal";
 
 const LeaderboardItem = ({ name, rank, badgecount, minutes, score }) => {
   const router = useRouter();
+  const border =
+    "p-[1px] bg-gradient-radial from-borderGradient-primary to-borderGradient-secondary";
+  const gradientBackground = "button-gradient bg-size-200 bg-pos-0";
+  const radialBackground =
+    "bg-gradient-radial from-cardGradient-primary to-cardGradient-secondary";
+
   return (
-    <div className="table-row">
-      <div className="table-cell ...">{name} </div>
-      <div className="table-cell ...">{rank} </div>
-      <div className="table-cell ...">{badgecount} </div>
-      <div className="table-cell ...">{minutes} </div>
-      <div className="table-cell ...">{score} </div>
+    <div
+      className={`flex flex-nowrap rounded-lg ${border} h-20 content-center`}
+    >
+      <div className={`w-[20%] px-5 text-left justify-self-center`}>{name}</div>
+      <div className={`w-[20%] px-5 text-left text-green-500 text-green-500`}>
+        {"+" + rank}
+      </div>
+      <div className={`w-[20%] px-5 text-left`}>{badgecount} </div>
+      <div className={`w-[20%] px-5 text-left`}>
+        {Math.floor(minutes / 60) + ":" + (minutes % 60)}
+      </div>
+      <div className={`w-[20%] px-5 text-left`}>{score} </div>
     </div>
   );
 };
--- src/components/profile/leaderboard/leaderboard-list.tsx
+++ src/components/profile/leaderboard/leaderboard-list.tsx
@@ -2,11 +2,6 @@ import { useState } from "react";
 import LeaderboardItem from "./leaderboard-item";
 
 const LeaderboardList = () => {
-  const border =
-    "p-[1px] bg-gradient-radial from-borderGradient-primary to-borderGradient-secondary";
-  const gradientBackground = "button-gradient bg-size-200 bg-pos-0";
-  const radialBackground =
-    "bg-gradient-radial from-cardGradient-primary to-cardGradient-secondary";
   const [leaderboardMockData] = useState([
     {
       name: "Tim Roberts",
@@ -16,14 +11,14 @@ const LeaderboardList = () => {
       score: 124
     },
     {
-      name: "Tim Short",
+      name: "Johnny Appleseed",
       rank: 3,
       badgeCount: 3,
       minutes: 516,
       score: 87
     },
     {
-      name: "Tim Short",
+      name: "Nick James",
       rank: 4,
       badgeCount: 1,
       minutes: 824,
@@ -31,34 +26,36 @@ const LeaderboardList = () => {
     }
   ]);
 
+  const border =
+    "p-[1px] bg-gradient-radial from-borderGradient-primary to-borderGradient-secondary";
+  const gradientBackground = "button-gradient bg-size-200 bg-pos-0";
+  const radialBackground =
+    "bg-gradient-radial from-cardGradient-primary to-cardGradient-secondary";
+
   return (
-    <div className="table w-full ...">
-      <div className="table-header-group ...">
-        <div className="table-row">
-          <div className="table-cell text-left ...">Name</div>
-          <div className="table-cell text-left ...">Rank</div>
-          <div className="table-cell text-left ...">Badges</div>
-          <div className="table-cell text-left ...">Hours</div>
-          <div className="table-cell text-left ...">Score</div>
-        </div>
+    <div className={`flex-nowrap space-y-4 w-[90%] `}>
+      <div className={`flex flex-nowrap rounded w-full `}>
+        <div className={`w-[20%] px-5 text-left`}>Leaderboard</div>
+        <div className={`w-[20%] px-5 text-left`}>Rank</div>
+        <div className={`w-[20%] px-5 text-left`}>Badges</div>
+        <div className={`w-[20%] px-5 text-left`}>Hours</div>
+        <div className={`w-[20%] px-5 text-left`}>Score</div>
       </div>
 
-      <div className="table-row-group">
-        {leaderboardMockData.map(
-          ({ name, rank, badgeCount, minutes, score }, index) => {
-            return (
-              <LeaderboardItem
-                key={index}
-                name={name}
-                rank={rank}
-                badgecount={badgeCount}
-                minutes={minutes}
-                score={score}
-              />
-            );
-          }
-        )}
-      </div>
+      {leaderboardMockData.map(
+        ({ name, rank, badgeCount, minutes, score }, index) => {
+          return (
+            <LeaderboardItem
+              key={index}
+              name={name}
+              rank={rank}
+              badgecount={badgeCount}
+              minutes={minutes}
+              score={score}
+            />
+          );
+        }
+      )}
     </div>
   );
 };
