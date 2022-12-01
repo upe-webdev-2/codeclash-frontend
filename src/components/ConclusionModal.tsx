61ea2cb2210d582fd3c97b68a10f2191c436939a
02danieljonas <02danieljonas@gmail.com>
2022-12-01 15:46:14 -0500

    added more information to the modal

--- src/components/ConclusionModal.tsx
+++ src/components/ConclusionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Dispatch, SetStateAction } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import Button from "./Button";
@@ -9,6 +9,8 @@ type ConclusionModalProps = {
   profileImage: string;
   didIWin: boolean;
   displayModal: boolean;
+  setDisplayModal: Dispatch<SetStateAction<boolean>>;
+  difficulty: "easy" | "medium" | "hard";
 };
 
 const ConclusionModal = ({
@@ -16,39 +18,60 @@ const ConclusionModal = ({
   profileImage,
   didIWin,
   displayModal,
+  difficulty,
+  setDisplayModal
 }: ConclusionModalProps) => {
   const { data } = useSession();
 
   const handleClose = () => {
-    console.log("Close");
+    setDisplayModal(false);
   };
 
-  useEffect(()=>{
-    
-  })
+  useEffect(() => {});
 
   return (
-    <div className="fixed rounded-[15px] z-50 w-96 bg-primary top-[50vh] left-[50vw] -translate-x-2/4 -translate-y-2/4 border border-blue-500/30 p-5 child:self-center flex    flex-col child:my-1">
+    <div className={displayModal ? "inline-block fixed z-50" : "hidden fixed z-50"}>
       <div
-        className="absolute right-[10px] top-[10px] rounded-full hover:bg-blue-400/10 cursor-pointer p-1"
         onClick={handleClose}
-      >
-        <AiOutlineClose />
-      </div>
+        className="cursor-pointer fixed top-0 right-0 left-0 bottom-0 z-[49] bg-black opacity-70"
+      ></div>
+      <div className="fixed rounded-[15px] z-50 w-[30rem] bg-primary top-[50vh] left-[50vw] -translate-x-2/4 -translate-y-2/4 border border-blue-500/30 p-5 child:self-center flex-col child:my-1 flex">
+        <div
+          className="absolute right-[10px] top-[10px] rounded-full hover:bg-blue-400/10 cursor-pointer p-2"
+          onClick={handleClose}
+        >
+          <AiOutlineClose size={20} />
+        </div>
 
-      <div>{name}</div>
-      {didIWin ? <div className="text-green-500">Congratulations! You Won!</div> : <div className="text-red-500">You lost!</div>}
-      <Button onclick={() => {}} type={"outline"} movingGradient={true}>
-        Play Again
-      </Button>
-      <Image
-        className="rounded-full"
-        alt="winner's profile picture"
-        src={profileImage}
-        width="161px"
-        height={"161px"}
-      />
-      <div></div>
+        <div className="text-5xl pb-1 font-gilroy-bold">{name}</div>
+
+        <div className="text-xl pb-5 font-gilroy">
+          {didIWin ? (
+            <p className="text-green-500">Congratulations! You Won!</p>
+          ) : (
+            <p className="text-red-500">You lost!</p>
+          )}
+        </div>
+        <Image
+          className="rounded-full"
+          alt="winner's profile picture"
+          src={profileImage}
+          width="150px"
+          height={"150px"}
+        />
+        <Button
+          style={{}}
+          onclick={() => {}}
+          type={"none"}
+          movingGradient={true}
+        >
+          View Your Profile
+        </Button>
+        <Button onclick={() => {}} type={"outline"} movingGradient={true}>
+          Play Again
+        </Button>
+        <div></div>
+      </div>
     </div>
   );
 };
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -15,6 +15,12 @@ const Dom = () => {
     console.log(`Searching for ${difficulty}`);
     setLoading(true);
   };
+  const [displayModal, setDisplayModal] = useState<boolean>(true);
+  const [didIWin, setDidIWin] = useState<boolean>(true);
+
+
+  console.log(useSession());
+  
 
   const cancelSearchForGame = () => {
     /**
@@ -27,7 +33,34 @@ const Dom = () => {
     <>
       <div className="w-screen">
         <Navbar />
-        <ConclusionModal displayModal={true} name={"Daniel"} profileImage={useSession()?.data?.user?.image || "https://avatars.githubusercontent.com/u/83048344?v=4"} didIWin={true} />
+        <div className="fixed z-[100]">
+          <button
+            onClick={() => {
+              setDisplayModal(!displayModal);
+            }}
+          >
+            Flip Display
+          </button>{" "}
+          |{" "}
+          <button
+            onClick={() => {
+              setDidIWin(!didIWin);
+            }}
+          >
+            Flip Win
+          </button>
+        </div>
+        <ConclusionModal
+          difficulty={"easy"}
+          displayModal={displayModal}
+          setDisplayModal={setDisplayModal}
+          name={"Daniel"}
+          profileImage={
+            useSession()?.data?.user?.image ||
+            "https://avatars.githubusercontent.com/u/83048344?v=4"
+          }
+          didIWin={didIWin}
+        />
 
         {isLoading ? (
           <Loading onCancel={cancelSearchForGame} />
