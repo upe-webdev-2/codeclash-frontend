4357053f22a510f65c3ce8c0b0831f4a1bfa2ca7
02danieljonas <02danieljonas@gmail.com>
2022-12-01 22:34:09 -0500

    adding our logo to favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,13 +13,13 @@ export default function Document() {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/static/logo.svg"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/static/logo.svg"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
